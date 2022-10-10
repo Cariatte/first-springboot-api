9d04f6344dc49ee3aeb9c6ff69b0be7a5660f5ea
Daniel Cariatte <nielcariatte@gmail.com>
2022-10-10 09:20:51 -0300

    implementando error para delete

--- src/main/java/br/com/magnasistemas/course/services/UserService.java
+++ src/main/java/br/com/magnasistemas/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.magnasistemas.course.entities.User;
 import br.com.magnasistemas.course.repositories.UserRepository;
+import br.com.magnasistemas.course.services.exceptions.DatabaseException;
 import br.com.magnasistemas.course.services.exceptions.ResourceNotFoundException;
 
 
@@ -34,7 +37,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
